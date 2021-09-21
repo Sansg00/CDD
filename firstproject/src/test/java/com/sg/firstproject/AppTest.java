28f8df741416dfa27c78ab7928e68d912514cc83
Sansg00 <88130747+Sansg00@users.noreply.github.com>
2021-09-21 21:41:59 +0530

    Update AppTest.java

--- firstproject/src/test/java/com/sg/firstproject/AppTest.java
+++ firstproject/src/test/java/com/sg/firstproject/AppTest.java
@@ -35,4 +35,21 @@ public class AppTest
     {
         assertTrue( true );
     }
+    public void testApp1()
+    {
+        assertTrue( true );
+    }
+    public void testApp2()
+    {
+        assertTrue( true );
+    }
+    public void testApp3()
+    {
+        assertTrue( true );
+    }
+    public void testApp4()
+    {
+        assertTrue( true );
+    }
+    
 }
